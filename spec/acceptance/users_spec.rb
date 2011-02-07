0a06feeb97582797fe2a57f61fe78d248859d71f
Akira Matsuda <ronnie@dio.jp>
2011-02-07 22:37:31 +0900

    r g steak:spec users

---
+++ spec/acceptance/users_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
+
+feature "Users", %q{
+  In order to ...
+  As a ...
+  I want to ...
+} do
+
+  scenario "Scenario name" do
+    true.should == true
+  end
+end
