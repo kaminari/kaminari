9a3c0b00f8e437443e97968de5e03cf0f6b2decb
Akira Matsuda <ronnie@dio.jp>
2011-02-07 11:32:27 +0900

    Haml generator

--- lib/generators/kaminari/views_generator.rb
+++ lib/generators/kaminari/views_generator.rb
@@ -1,11 +1,20 @@
 module Kaminari
   module Generators
     class ViewsGenerator < Rails::Generators::Base
-      source_root File.expand_path('../../../../app/views', __FILE__)
+      source_root File.expand_path('../../../../app/views/kaminari', __FILE__)
+
+      class_option :template_engine, :type => :string, :aliases => '-e', :desc => 'Template engine for the views. Available options are "erb" and "haml".'
 
       desc 'Copies all paginator partials to your application.'
       def copy_views
-        directory 'kaminari', 'app/views/kaminari'
+        Dir.glob(filename_pattern).map {|f| File.basename f}.each do |f|
+          copy_file f, "app/views/kaminari/#{f}"
+        end
+      end
+
+      private
+      def filename_pattern
+        File.join self.class.source_root, "*.html.#{options[:template_engine] || 'erb'}"
       end
     end
   end
