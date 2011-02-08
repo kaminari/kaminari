af61af91329fcea825defb787ec25c9ba169cbcc
Akira Matsuda <ronnie@dio.jp>
2011-02-08 11:21:39 +0900

    introducing Steak

--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -1,12 +1,43 @@
 require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
-feature "Users", %q{
-  In order to ...
-  As a ...
-  I want to ...
-} do
+feature 'Users' do
+  background do
+    1.upto(100) {|i| User.create! :name => "user#{'%03d' % i}" }
+  end
+  scenario 'navigating by pagination links' do
+    visit users_path
 
-  scenario "Scenario name" do
-    true.should == true
+    within 'div.pagination' do
+      within 'span.page.current' do
+        page.should have_content '1'
+      end
+      within 'span.next' do
+        click_link 'Next »'
+      end
+    end
+
+    within 'div.pagination' do
+      within 'span.page.current' do
+        page.should have_content '2'
+      end
+      within 'span.page.last' do
+        click_link '4'
+      end
+    end
+
+    within 'div.pagination' do
+      within 'span.page.current' do
+        page.should have_content '4'
+      end
+      within 'span.prev' do
+        click_link '« Prev'
+      end
+    end
+
+    within 'div.pagination' do
+      within 'span.page.current' do
+        page.should have_content '3'
+      end
+    end
   end
 end
