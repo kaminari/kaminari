4f39f57068be54234a37a11fa1f31292e542acd4
Akira Matsuda <ronnie@dio.jp>
2011-02-10 05:28:50 +0900

    githubish theme...

--- lib/generators/kaminari/views_generator.rb
+++ lib/generators/kaminari/views_generator.rb
@@ -16,6 +16,7 @@ rails g kaminari:views THEME [options]
                  This one is used internally while you don't override the partials.
         google:  Looks googlish! (note that this is just an example...)
                  Make sure to specify a very big number (like 65536) for the "window" value.
+        github:  A very simple one with only "Older" and "Newer" links.
 BANNER
       end
 
