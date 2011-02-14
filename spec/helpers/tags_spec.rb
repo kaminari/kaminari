8a3803697fb7c03947f677e77dc46927486296b7
Akira Matsuda <ronnie@dio.jp>
2011-02-14 11:23:52 +0900

    I said, Tag is not Renderable

--- spec/helpers/tags_spec.rb
+++ spec/helpers/tags_spec.rb
@@ -3,49 +3,45 @@ include Kaminari::Helpers
 
 describe 'Kaminari::Helpers' do
   describe 'template lookup rule' do
-    describe 'Tag' do
-      subject { Tag }
-      its(:ancestor_renderables) { should == [Tag] }
-    end
     describe 'Paginator' do
       subject { Paginator }
-      its(:ancestor_renderables) { should == [Paginator, Tag] }
+      its(:ancestor_renderables) { should == [Paginator] }
     end
     describe 'PrevLink' do
       subject { PrevLink }
-      its(:ancestor_renderables) { should == [PrevLink, Prev, Link, Page, Tag] }
+      its(:ancestor_renderables) { should == [PrevLink, Prev, Link, Page] }
     end
     describe 'PrevSpan' do
       subject { PrevSpan }
-      its(:ancestor_renderables) { should == [PrevSpan, Prev, NonLink, Tag] }
+      its(:ancestor_renderables) { should == [PrevSpan, Prev, NonLink] }
     end
     describe 'FirstPageLink' do
       subject { FirstPageLink }
-      its(:ancestor_renderables) { should == [FirstPageLink, PageLink, Link, Page, Tag] }
+      its(:ancestor_renderables) { should == [FirstPageLink, PageLink, Link, Page] }
     end
     describe 'PageLink' do
       subject { PageLink }
-      its(:ancestor_renderables) { should == [PageLink, Link, Page, Tag] }
+      its(:ancestor_renderables) { should == [PageLink, Link, Page] }
     end
     describe 'CurrentPage' do
       subject { CurrentPage }
-      its(:ancestor_renderables) { should == [CurrentPage, NonLink, Page, Tag] }
+      its(:ancestor_renderables) { should == [CurrentPage, NonLink, Page] }
     end
     describe 'TruncatedSpan' do
       subject { TruncatedSpan }
-      its(:ancestor_renderables) { should == [TruncatedSpan, NonLink, Tag] }
+      its(:ancestor_renderables) { should == [TruncatedSpan, NonLink] }
     end
     describe 'LastPageLink' do
       subject { LastPageLink }
-      its(:ancestor_renderables) { should == [LastPageLink, PageLink, Link, Page, Tag] }
+      its(:ancestor_renderables) { should == [LastPageLink, PageLink, Link, Page] }
     end
     describe 'NextLink' do
       subject { NextLink }
-      its(:ancestor_renderables) { should == [NextLink, Next, Link, Page, Tag] }
+      its(:ancestor_renderables) { should == [NextLink, Next, Link, Page] }
     end
     describe 'NextSpan' do
       subject { NextSpan }
-      its(:ancestor_renderables) { should == [NextSpan, Next, NonLink, Tag] }
+      its(:ancestor_renderables) { should == [NextSpan, Next, NonLink] }
     end
   end
 end
