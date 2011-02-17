82dc93182caf675461a101e4ae31e5258bdb0b1b
Akira Matsuda <ronnie@dio.jp>
2011-02-18 04:29:38 +0900

    load dependencies inside the initializer
    otherwise, the orm would possibly not yet loaded

--- lib/kaminari/railtie.rb
+++ lib/kaminari/railtie.rb
@@ -1,18 +1,22 @@
 require 'rails'
 require 'action_view'
-require File.join(File.dirname(__FILE__), 'active_record') if defined? ::ActiveRecord
-require File.join(File.dirname(__FILE__), 'mongoid') if defined? ::Mongoid
-require File.join(File.dirname(__FILE__), 'mongoid/criteria') if defined? ::Mongoid::Criteria
-require File.join(File.dirname(__FILE__), 'mongoid/document') if defined? ::Mongoid::Document
 require File.join(File.dirname(__FILE__), 'helpers')
 
 module Kaminari
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'paginatablize' do |app|
-      ::ActiveRecord::Base.send :include, Kaminari::ActiveRecord if defined? ::ActiveRecord::Base
+      if defined? ::ActiveRecord
+        require File.join(File.dirname(__FILE__), 'active_record')
+        ::ActiveRecord::Base.send :include, Kaminari::ActiveRecord
+      end
+      if defined? ::Mongoid
+        require File.join(File.dirname(__FILE__), 'mongoid')
+        require File.join(File.dirname(__FILE__), 'mongoid/criteria')
+        require File.join(File.dirname(__FILE__), 'mongoid/document')
+        ::Mongoid::Criteria.send :include, Kaminari::Mongoid::Criteria
+        ::Mongoid::Document.send :include, Kaminari::Mongoid::Document
+      end
       ::ActionView::Base.send :include, Kaminari::Helpers
-      ::Mongoid::Criteria.send :include, Kaminari::Mongoid::Criteria if defined? ::Mongoid::Criteria
-      ::Mongoid::Document.send :include, Kaminari::Mongoid::Document if defined? ::Mongoid::Document
     end
   end
 end
