34f75fdbde042a09474ec61c0698b4bc62ef8493
Akira Matsuda <ronnie@dio.jp>
2011-02-18 01:00:21 +0900

    stop depending on ActiveRecord

--- lib/kaminari/railtie.rb
+++ lib/kaminari/railtie.rb
@@ -1,13 +1,12 @@
 require 'rails'
-require 'active_record'
 require 'action_view'
-require File.join(File.dirname(__FILE__), 'active_record')
+require File.join(File.dirname(__FILE__), 'active_record') if defined? ::ActiveRecord
 require File.join(File.dirname(__FILE__), 'helpers')
 
 module Kaminari
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'paginatablize' do |app|
-      ::ActiveRecord::Base.send :include, Kaminari::ActiveRecord
+      ::ActiveRecord::Base.send :include, Kaminari::ActiveRecord if defined? ::ActiveRecord::Base
       ::ActionView::Base.send :include, Kaminari::Helpers
     end
   end
