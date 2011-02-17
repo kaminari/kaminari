88f4fbe09c5f25e6b22da0c69cb1b171316d0699
Junya Ogura <junyaogura@gmail.com>
2011-02-18 03:37:52 +0900

    care mongoid dependency

--- lib/kaminari/railtie.rb
+++ lib/kaminari/railtie.rb
@@ -2,15 +2,17 @@ require 'rails'
 require 'action_view'
 require File.join(File.dirname(__FILE__), 'active_record') if defined? ::ActiveRecord
 require File.join(File.dirname(__FILE__), 'mongoid') if defined? ::Mongoid
+require File.join(File.dirname(__FILE__), 'mongoid/criteria') if defined? ::Mongoid::Criteria
+require File.join(File.dirname(__FILE__), 'mongoid/document') if defined? ::Mongoid::Document
 require File.join(File.dirname(__FILE__), 'helpers')
-require File.join(File.dirname(__FILE__), 'mongoid')
 
 module Kaminari
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'paginatablize' do |app|
       ::ActiveRecord::Base.send :include, Kaminari::ActiveRecord if defined? ::ActiveRecord::Base
       ::ActionView::Base.send :include, Kaminari::Helpers
-      ::Mongoid::Document.send :include, Kaminari::Mongoid if defined? ::Mongoid::Document
+      ::Mongoid::Criteria.send :include, Kaminari::Mongoid::Criteria if defined? ::Mongoid::Criteria
+      ::Mongoid::Document.send :include, Kaminari::Mongoid::Document if defined? ::Mongoid::Document
     end
   end
 end
