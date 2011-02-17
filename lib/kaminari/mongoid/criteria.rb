32300200a9a51c1cd49d93d1eca5431942091c64
Akira Matsuda <ronnie@dio.jp>
2011-02-18 07:12:17 +0900

    DRY them up

--- lib/kaminari/mongoid/criteria.rb
+++ lib/kaminari/mongoid/criteria.rb
@@ -4,35 +4,7 @@ module Kaminari
       extend ActiveSupport::Concern
 
       included do
-        # Fetch the values at the specified page number
-        #   Model.order_by(:name.asc).page(5)
-        def page(num)
-          limit(@klass.default_per_page).offset(@klass.default_per_page * ([num.to_i, 1].max - 1))
-        end
-
-        # Specify the <tt>per_page</tt> value for the preceding <tt>page</tt> scope
-        #   Model.order_by(:name.asc).page(3).per(10)
-        def per(num)
-          if (n = num.to_i) <= 0
-            self
-          else
-            limit(n).offset(options[:skip] / options[:limit] * n)
-          end
-        end
-
-        # Total number of pages
-        def num_pages
-          (count.to_f / options[:limit]).ceil
-        end
-
-        # Current page number
-        def current_page
-          (options[:skip] / options[:limit]) + 1
-        end
-
-        def limit_value
-          options[:limit]
-        end
+        delegate :page, :per, :num_pages, :current_page, :limit_value, :to => '@klass'
       end
     end
   end
