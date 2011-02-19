5a7e932d8a6b1452d4115d89c3dcfd35e0bc4c8d
Akira Matsuda <ronnie@dio.jp>
2011-02-19 14:40:47 +0900

    better initializer name

--- lib/kaminari/railtie.rb
+++ lib/kaminari/railtie.rb
@@ -8,7 +8,7 @@ require File.join(File.dirname(__FILE__), 'helpers')
 
 module Kaminari
   class Railtie < ::Rails::Railtie #:nodoc:
-    initializer 'paginatablize' do |app|
+    initializer 'kaminari' do |app|
       if defined? ::ActiveRecord
         require File.join(File.dirname(__FILE__), 'active_record_extension')
         ::ActiveRecord::Base.send :include, Kaminari::ActiveRecordExtension
