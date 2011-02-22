062c60344b0fb66999111f08d2d4865767077594
Akira Matsuda <ronnie@dio.jp>
2011-02-22 22:18:14 +0900

    fixes #21 ensure total_count returns a number
    Model.group.count does not return a Fixnum but an OrderdHash

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -3,7 +3,9 @@ module Kaminari
     extend ActiveSupport::Concern
     module InstanceMethods
       def total_count #:nodoc:
-        except(:offset, :limit).count
+        c = except(:offset, :limit).count
+        # .group returns an OrderdHash that responds to #count
+        c.respond_to?(:count) ? c.count : c
       end
     end
   end
