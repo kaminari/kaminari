ed4b22d44d40efdcac8ecb4f198424beaf6dfe83
Aurélien AMILIN <aurelien.amilin@gmail.com>
2011-03-03 20:06:40 +0100

    exclude :includes in count

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -3,7 +3,7 @@ module Kaminari
     extend ActiveSupport::Concern
     module InstanceMethods
       def total_count #:nodoc:
-        c = except(:offset, :limit).count
+        c = except(:offset, :limit, :includes).count
         # .group returns an OrderdHash that responds to #count
         c.respond_to?(:count) ? c.count : c
       end
