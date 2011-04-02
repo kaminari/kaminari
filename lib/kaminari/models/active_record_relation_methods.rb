2fcaf14a0351ce825ba64617984e9730a627fb9f
Ben Woosley <ben.woosley@gmail.com>
2011-04-01 22:54:04 -0700

    Skip :order in #count_all so complex groups with generated columns don't blow up in SQL-land

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -9,6 +9,7 @@ module Kaminari
       end
 
       def total_count #:nodoc:
+        # #count overrides the #select which could include generated columns referenced in #order, so skip #order here, where it's irrelevant to the result anyway
         c = except(:offset, :limit, :order).count
         # .group returns an OrderdHash that responds to #count
         c.respond_to?(:count) ? c.count : c
