749c04dac607cecdb1bea3a59a9ad8a991fac582
Akira Matsuda <ronnie@dio.jp>
2011-04-16 19:28:17 +0900

    a constant for Kaminari version

--- lib/kaminari.rb
+++ lib/kaminari.rb
@@ -1,2 +1,3 @@
+require File.join(File.dirname(__FILE__), 'kaminari/version')
 require File.join(File.dirname(__FILE__), 'kaminari/railtie')
 require File.join(File.dirname(__FILE__), 'kaminari/engine')
---
+++ lib/kaminari/version.rb
@@ -0,0 +1,3 @@
+module Kaminari
+  VERSION = File.read File.join(File.dirname(__FILE__), '../../VERSION')
+end
