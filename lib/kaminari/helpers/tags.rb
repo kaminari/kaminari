e6dc0227207e913b7ba8f3ed6823eb0799e7446b
Akira Matsuda <ronnie@dio.jp>
2011-04-19 19:28:00 +0900

    Fix Issue 59: Allow for Multiple Themes (thanks @tmilewski)

--- lib/kaminari/helpers/tags.rb
+++ lib/kaminari/helpers/tags.rb
@@ -16,11 +16,12 @@ module Kaminari
       def initialize(template, options = {}) #:nodoc:
         @template, @options = template, options.dup
         @param_name = @options.delete(:param_name)
+        @theme = @options[:theme] ? "#{@options.delete(:theme)}/" : ''
         @params = @options[:params] ? template.params.merge(@options.delete :params) : template.params
       end
 
       def to_s(locals = {}) #:nodoc:
-        @template.render :partial => "kaminari/#{self.class.name.demodulize.underscore}", :locals => @options.merge(locals)
+        @template.render :partial => "kaminari/#{@theme}#{self.class.name.demodulize.underscore}", :locals => @options.merge(locals)
       end
 
       def page_url_for(page)
