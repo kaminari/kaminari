0c4832fca6423cdfd9cfef86cf157f5ef1469b27
Andrew Kane <acekane1@gmail.com>
2011-04-20 18:02:23 -0700

    Calling length instead of count is super-inefficient and does not work with larger databases

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -2,14 +2,8 @@ module Kaminari
   module ActiveRecordRelationMethods
     extend ActiveSupport::Concern
     module InstanceMethods
-      if Rails.version < '3.1'
-        def count #:nodoc:
-          limit_value == 0 ? 0 : length
-        end
-      end
-
       def total_count #:nodoc:
-        # #count overrides the #select which could include generated columns referenced in #order, so skip #order here, where it's irrelevant to the result anyway
+        # count overrides the #select which could include generated columns referenced in #order, so skip #order here, where it's irrelevant to the result anyway
         c = except(:offset, :limit, :includes, :order).count
         # .group returns an OrderdHash that responds to #count
         c.respond_to?(:count) ? c.count : c
