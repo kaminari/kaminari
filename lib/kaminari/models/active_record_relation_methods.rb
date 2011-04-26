5a40939e4b63ea009a4ba9edaf52f32790e188a0
Florent Piteau <florent.piteau@sylogix.net>
2011-04-26 16:58:34 +0200

    Only remove includes from count if they are irrelevant.

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -12,8 +12,11 @@ module Kaminari
 
       def total_count #:nodoc:
         # #count overrides the #select which could include generated columns referenced in #order, so skip #order here, where it's irrelevant to the result anyway
-        c = except(:offset, :limit, :includes, :order).count
+        c = except(:offset, :limit, :order)
+        # Remove includes only if they are irrelevant
+        c = c.except(:includes) unless references_eager_loaded_tables?
         # .group returns an OrderdHash that responds to #count
+        c = c.count
         c.respond_to?(:count) ? c.count : c
       end
     end
