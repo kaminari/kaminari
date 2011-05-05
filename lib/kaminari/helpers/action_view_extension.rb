c96d77f5d80cf3db4a72594ba69b6914fe849d8d
Akira Matsuda <ronnie@dio.jp>
2011-05-05 23:41:17 +0900

    a simple "Twitter like" pagination link that creates a link to the next page
    via http://stackoverflow.com/questions/5734597/rails-3-more-ajax-pagination-with-kaminari

--- lib/kaminari/helpers/action_view_extension.rb
+++ lib/kaminari/helpers/action_view_extension.rb
@@ -1,9 +1,8 @@
 module Kaminari
+  # = Helpers
   module ActionViewExtension
-    extend ActiveSupport::Concern
+    extend ::ActiveSupport::Concern
     module InstanceMethods
-      # = Helpers
-      #
       # A helper that renders the pagination links.
       #
       #   <%= paginate @articles %>
@@ -20,6 +19,31 @@ module Kaminari
       def paginate(scope, options = {}, &block)
         paginator = Kaminari::Helpers::Paginator.new self, options.reverse_merge(:current_page => scope.current_page, :num_pages => scope.num_pages, :per_page => scope.limit_value, :param_name => Kaminari.config.param_name, :remote => false)
         paginator.to_s
+      end
+
+      # A simple "Twitter like" pagination link that creates a link to the next page.
+      #
+      # ==== Examples
+      # Basic usage:
+      #
+      #   <%= link_to_next_page @items, 'Next Page' %>
+      #
+      # Ajax:
+      #
+      #   <%= link_to_next_page @items, 'Next Page', :remote => true %>
+      #
+      # By default, it renders nothing if there are no more results on the next page.
+      # You can customize this output by passing a block.
+      #
+      #   <%= link_to_next_page @users, 'Next Page' do %>
+      #     <span>No More Pages</span>
+      #   <% end %>
+      def link_to_next_page(scope, name, options = {}, &block)
+        params = options.delete(:params) || {}
+        param_name = options.delete(:param_name) || Kaminari.config.param_name
+        link_to_unless scope.last_page?, name, params.merge(param_name => (scope.current_page + 1)), options.merge(:rel => 'next') do
+          block.call if block
+        end
       end
     end
   end
--- spec/helpers/action_view_extension_spec.rb
+++ spec/helpers/action_view_extension_spec.rb
@@ -1,9 +1,11 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 
 describe 'Kaminari::ActionViewExtension' do
+  before do
+    50.times {|i| User.create! :name => "user#{i}"}
+  end
   describe '#paginate' do
     before do
-      50.times {|i| User.create! :name => "user#{i}"}
       @users = User.page(1)
     end
     subject { helper.paginate @users, :params => {:controller => 'users', :action => 'index'} }
@@ -13,6 +15,23 @@ describe 'Kaminari::ActionViewExtension' do
       it 'should escape for javascript' do
         lambda { escape_javascript(helper.paginate @users, :params => {:controller => 'users', :action => 'index'}) }.should_not raise_error
       end
+    end
+  end
+
+  describe '#link_to_next_page' do
+    context 'having more page' do
+      before do
+        @users = User.page(1)
+      end
+      subject { helper.link_to_next_page @users, 'More', :params => {:controller => 'users', :action => 'index'} }
+      it { should be_a String }
+    end
+    context 'the last page' do
+      before do
+        @users = User.page(2)
+      end
+      subject { helper.link_to_next_page @users, 'More', :params => {:controller => 'users', :action => 'index'} }
+      it { should_not be }
     end
   end
 end
