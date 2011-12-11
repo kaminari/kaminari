0a36c1793b16dad403186650dfa83c7b8fc06c3d
Akira Matsuda <ronnie@dio.jp>
2011-12-11 12:35:17 +0900

    refactor total_count memoization code
    @Empact "Would be nicer to use the "@total_count ||= begin" convention, IMO." https://github.com/amatsuda/kaminari/pull/138#issuecomment-1692686

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -12,15 +12,16 @@ module Kaminari
 
       def total_count #:nodoc:
         # #count overrides the #select which could include generated columns referenced in #order, so skip #order here, where it's irrelevant to the result anyway
-        return @total_count if @total_count
-        c = except(:offset, :limit, :order)
-        # a workaround for 3.1.beta1 bug. see: https://github.com/rails/rails/issues/406
-        c = c.reorder nil
-        # Remove includes only if they are irrelevant
-        c = c.except(:includes) unless references_eager_loaded_tables?
-        # .group returns an OrderdHash that responds to #count
-        c = c.count
-        @total_count = c.respond_to?(:count) ? c.count : c
+        @total_count ||= begin
+          c = except(:offset, :limit, :order)
+          # a workaround for 3.1.beta1 bug. see: https://github.com/rails/rails/issues/406
+          c = c.reorder nil
+          # Remove includes only if they are irrelevant
+          c = c.except(:includes) unless references_eager_loaded_tables?
+          # .group returns an OrderdHash that responds to #count
+          c = c.count
+          @total_count = c.respond_to?(:count) ? c.count : c
+        end
       end
     end
   end
