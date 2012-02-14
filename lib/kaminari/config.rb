7cb1827d54bac1fa498f3b452702b5f2b1995495
Akira Matsuda <ronnie@dio.jp>
2012-02-14 19:21:53 +0900

    avoid "method redefined" warning

--- lib/kaminari/config.rb
+++ lib/kaminari/config.rb
@@ -23,11 +23,15 @@ module Kaminari
     config_accessor :left
     config_accessor :right
     config_accessor :page_method_name
-    config_accessor :param_name
 
     def param_name
       config.param_name.respond_to?(:call) ? config.param_name.call : config.param_name
     end
+
+    # define param_name writer (copied from AS::Configurable)
+    writer, line = 'def param_name=(value); config.param_name = value; end', __LINE__
+    singleton_class.class_eval writer, __FILE__, line
+    class_eval writer, __FILE__, line
   end
 
   # this is ugly. why can't we pass the default value to config_accessor...?
