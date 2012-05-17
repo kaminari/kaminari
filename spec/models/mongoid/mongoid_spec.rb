854d04af4b648299fd16ed2506959af1d57f7940
Akira Matsuda <ronnie@dio.jp>
2012-05-17 21:08:02 +0900

    [AR, DM, Mongoid, MongoMapper].each {|orm| run orm specs only if orm is loaded }

--- spec/models/mongoid/mongoid_spec.rb
+++ spec/models/mongoid/mongoid_spec.rb
@@ -1,145 +1,148 @@
 require 'spec_helper'
-require 'mongoid'
-require 'kaminari/models/mongoid_extension'
 
-describe Kaminari::MongoidExtension do
+if defined? Mongoid
+  require 'mongoid'
+  require 'kaminari/models/mongoid_extension'
 
-  before do
-    begin
-      Mongoid.configure do |config|
+  describe Kaminari::MongoidExtension do
+
+    before do
+      begin
+        Mongoid.configure do |config|
+          if Mongoid::VERSION =~ /^3/
+            config.sessions = { default: { hosts: [ "localhost:27017" ], database: 'kaminari_test' }}
+          else
+            config.master = Mongo::Connection.new.db("kaminari_test")
+          end
+        end
+        class MongoidExtensionDeveloper
+          include ::Mongoid::Document
+          field :salary, :type => Integer
+        end
+      rescue Mongo::ConnectionFailure
+        pending 'can not connect to MongoDB'
+      end
+    end
+
+    before(:each) do
+      MongoidExtensionDeveloper.all.destroy
+      41.times do
+        MongoidExtensionDeveloper.create!({:salary => 1})
+      end
+    end
+
+    describe '#page' do
+
+      context 'page 1' do
+        subject { MongoidExtensionDeveloper.page 1 }
+        it { should be_a Mongoid::Criteria }
+        its(:current_page) { should == 1 }
+        its(:limit_value) { should == 25 }
+        its(:num_pages) { should == 2 }
+        it { should skip(0) }
+      end
+
+      context 'page 2' do
+        subject { MongoidExtensionDeveloper.page 2 }
+        it { should be_a Mongoid::Criteria }
+        its(:current_page) { should == 2 }
+        its(:limit_value) { should == 25 }
+        its(:num_pages) { should == 2 }
+        it { should skip 25 }
+      end
+
+      context 'page "foobar"' do
+        subject { MongoidExtensionDeveloper.page 'foobar' }
+        it { should be_a Mongoid::Criteria }
+        its(:current_page) { should == 1 }
+        its(:limit_value) { should == 25 }
+        its(:num_pages) { should == 2 }
+        it { should skip 0 }
+      end
+
+      shared_examples 'complete valid pagination' do
         if Mongoid::VERSION =~ /^3/
-          config.sessions = { default: { hosts: [ "localhost:27017" ], database: 'kaminari_test' }}
+          its(:selector) { should == {'salary' => 1} }
         else
-          config.master = Mongo::Connection.new.db("kaminari_test")
+          its(:selector) { should == {:salary => 1} }
+        end
+        its(:current_page) { should == 2 }
+        its(:limit_value) { should == 25 }
+        its(:num_pages) { should == 2 }
+        it { should skip 25 }
+      end
+
+      context 'with criteria before' do
+        subject { MongoidExtensionDeveloper.where(:salary => 1).page 2 }
+        it_should_behave_like 'complete valid pagination'
+      end
+
+      context 'with criteria after' do
+        subject { MongoidExtensionDeveloper.page(2).where(:salary => 1) }
+        it_should_behave_like 'complete valid pagination'
+      end
+    end
+
+    describe '#per' do
+      subject { MongoidExtensionDeveloper.page(2).per(10) }
+      it { should be_a Mongoid::Criteria }
+      its(:current_page) { should == 2 }
+      its(:limit_value) { should == 10 }
+      its(:num_pages) { should == 5 }
+      it { should skip 10 }
+    end
+
+    describe '#page in embedded documents' do
+      before :all do
+        class MongoMongoidExtensionDeveloper
+          include ::Mongoid::Document
+          field :salary, :type => Integer
+          embeds_many :frameworks
+        end
+
+        class Framework
+          include ::Mongoid::Document
+          field :name, :type => String
+          field :language, :type => String
+          embedded_in :mongo_mongoid_extension_developer
         end
       end
-      class MongoidExtensionDeveloper
-        include ::Mongoid::Document
-        field :salary, :type => Integer
-      end
-    rescue Mongo::ConnectionFailure
-      pending 'can not connect to MongoDB'
-    end
-  end
 
-  before(:each) do
-    MongoidExtensionDeveloper.all.destroy
-    41.times do
-      MongoidExtensionDeveloper.create!({:salary => 1})
-    end
-  end
-
-  describe '#page' do
-
-    context 'page 1' do
-      subject { MongoidExtensionDeveloper.page 1 }
-      it { should be_a Mongoid::Criteria }
-      its(:current_page) { should == 1 }
-      its(:limit_value) { should == 25 }
-      its(:num_pages) { should == 2 }
-      it { should skip(0) }
-    end
-
-    context 'page 2' do
-      subject { MongoidExtensionDeveloper.page 2 }
-      it { should be_a Mongoid::Criteria }
-      its(:current_page) { should == 2 }
-      its(:limit_value) { should == 25 }
-      its(:num_pages) { should == 2 }
-      it { should skip 25 }
-    end
-
-    context 'page "foobar"' do
-      subject { MongoidExtensionDeveloper.page 'foobar' }
-      it { should be_a Mongoid::Criteria }
-      its(:current_page) { should == 1 }
-      its(:limit_value) { should == 25 }
-      its(:num_pages) { should == 2 }
-      it { should skip 0 }
-    end
-
-    shared_examples 'complete valid pagination' do
-      if Mongoid::VERSION =~ /^3/
-        its(:selector) { should == {'salary' => 1} }
-      else
-        its(:selector) { should == {:salary => 1} }
-      end
-      its(:current_page) { should == 2 }
-      its(:limit_value) { should == 25 }
-      its(:num_pages) { should == 2 }
-      it { should skip 25 }
-    end
-
-    context 'with criteria before' do
-      subject { MongoidExtensionDeveloper.where(:salary => 1).page 2 }
-      it_should_behave_like 'complete valid pagination'
-    end
-
-    context 'with criteria after' do
-      subject { MongoidExtensionDeveloper.page(2).where(:salary => 1) }
-      it_should_behave_like 'complete valid pagination'
-    end
-  end
-
-  describe '#per' do
-    subject { MongoidExtensionDeveloper.page(2).per(10) }
-    it { should be_a Mongoid::Criteria }
-    its(:current_page) { should == 2 }
-    its(:limit_value) { should == 10 }
-    its(:num_pages) { should == 5 }
-    it { should skip 10 }
-  end
-
-  describe '#page in embedded documents' do
-    before :all do
-      class MongoMongoidExtensionDeveloper
-        include ::Mongoid::Document
-        field :salary, :type => Integer
-        embeds_many :frameworks
+      before :all do
+        @mongo_developer = MongoMongoidExtensionDeveloper.new
+        @mongo_developer.frameworks.new(:name => "rails", :language => "ruby")
+        @mongo_developer.frameworks.new(:name => "merb", :language => "ruby")
+        @mongo_developer.frameworks.new(:name => "sinatra", :language => "ruby")
+        @mongo_developer.frameworks.new(:name => "cakephp", :language => "php")
+        @mongo_developer.frameworks.new(:name => "tornado", :language => "python")
       end
 
-      class Framework
-        include ::Mongoid::Document
-        field :name, :type => String
-        field :language, :type => String
-        embedded_in :mongo_mongoid_extension_developer
+      context 'page 1' do
+        subject { @mongo_developer.frameworks.page(1).per(1) }
+        it { should be_a Mongoid::Criteria }
+        its(:total_count) { should == 5 }
+        its(:limit_value) { should == 1 }
+        its(:current_page) { should == 1 }
+        its(:num_pages) { should == 5 }
       end
-    end
 
-    before :all do
-      @mongo_developer = MongoMongoidExtensionDeveloper.new
-      @mongo_developer.frameworks.new(:name => "rails", :language => "ruby")
-      @mongo_developer.frameworks.new(:name => "merb", :language => "ruby")
-      @mongo_developer.frameworks.new(:name => "sinatra", :language => "ruby")
-      @mongo_developer.frameworks.new(:name => "cakephp", :language => "php")
-      @mongo_developer.frameworks.new(:name => "tornado", :language => "python")
-    end
+      context 'with criteria after' do
+        subject { @mongo_developer.frameworks.page(1).per(2).where(:language => "ruby") }
+        it { should be_a Mongoid::Criteria }
+        its(:total_count) { should == 3 }
+        its(:limit_value) { should == 2 }
+        its(:current_page) { should == 1 }
+        its(:num_pages) { should == 2 }
+      end
 
-    context 'page 1' do
-      subject { @mongo_developer.frameworks.page(1).per(1) }
-      it { should be_a Mongoid::Criteria }
-      its(:total_count) { should == 5 }
-      its(:limit_value) { should == 1 }
-      its(:current_page) { should == 1 }
-      its(:num_pages) { should == 5 }
-    end
-
-    context 'with criteria after' do
-      subject { @mongo_developer.frameworks.page(1).per(2).where(:language => "ruby") }
-      it { should be_a Mongoid::Criteria }
-      its(:total_count) { should == 3 }
-      its(:limit_value) { should == 2 }
-      its(:current_page) { should == 1 }
-      its(:num_pages) { should == 2 }
-    end
-
-    context 'with criteria before' do
-      subject { @mongo_developer.frameworks.where(:language => "ruby").page(1).per(2) }
-      it { should be_a Mongoid::Criteria }
-      its(:total_count) { should == 3 }
-      its(:limit_value) { should == 2 }
-      its(:current_page) { should == 1 }
-      its(:num_pages) { should == 2 }
+      context 'with criteria before' do
+        subject { @mongo_developer.frameworks.where(:language => "ruby").page(1).per(2) }
+        it { should be_a Mongoid::Criteria }
+        its(:total_count) { should == 3 }
+        its(:limit_value) { should == 2 }
+        its(:current_page) { should == 1 }
+        its(:num_pages) { should == 2 }
+      end
     end
   end
 end
