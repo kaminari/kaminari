a87acfb76891c1467a57f9910600df86b14d8806
Akira Matsuda <ronnie@dio.jp>
2012-05-23 06:52:50 +0900

    let the specs run on Rails || Sinatra

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require 'rails'
+begin
+  require 'rails'
+rescue LoadError
+end
+
 require 'bundler/setup'
 Bundler.require
 
@@ -10,9 +14,15 @@ require 'database_cleaner'
 # Simulate a gem providing a subclass of ActiveRecord::Base before the Railtie is loaded.
 require 'fake_gem' if defined? ActiveRecord
 
-require 'fake_app/fake_app'
+if defined? Rails
+  require 'fake_app/fake_app'
 
-require 'rspec/rails'
+  require 'rspec/rails'
+end
+if defined? Sinatra
+  require 'spec_helper_for_sinatra'
+end
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
