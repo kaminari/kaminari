1c43100edd2a91653c3f1c404a46c0cd6d0db146
Jacek Jakubik <jakubik.jacek@gmail.com>
2012-07-18 15:52:13 +0200

    subclasses extensions are picked up when first model inherits from ar::base

--- lib/kaminari/models/active_record_extension.rb
+++ lib/kaminari/models/active_record_extension.rb
@@ -4,18 +4,21 @@ module Kaminari
   module ActiveRecordExtension
     extend ActiveSupport::Concern
     included do
+      @_subclasses_loaded = false
       # Future subclasses will pick up the model extension
       class << self
         def inherited_with_kaminari(kls) #:nodoc:
+          # First model should also force subclasses to pick up the model extension as well
+          unless @_subclasses_loaded
+            self.descendants.each do |kls|
+              kls.send(:include, Kaminari::ActiveRecordModelExtension) if kls.superclass == ActiveRecord::Base
+            end
+            @_subclasses_loaded = true
+          end
           inherited_without_kaminari kls
           kls.send(:include, Kaminari::ActiveRecordModelExtension) if kls.superclass == ActiveRecord::Base
         end
         alias_method_chain :inherited, :kaminari
-      end
-
-      # Existing subclasses pick up the model extension as well
-      self.descendants.each do |kls|
-        kls.send(:include, Kaminari::ActiveRecordModelExtension) if kls.superclass == ActiveRecord::Base
       end
     end
   end
