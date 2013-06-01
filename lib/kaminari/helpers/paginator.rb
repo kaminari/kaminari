4f847d09811b4534a128ab7f6bb9df4ad8fc8419
Joshua Kovach <kovach.jc@gmail.com>
2013-06-01 15:57:50 -0400

    Allow exclusion of navigation links via #paginate

--- lib/kaminari/helpers/paginator.rb
+++ lib/kaminari/helpers/paginator.rb
@@ -23,6 +23,7 @@ module Kaminari
         @template, @options = template, options
         @theme = @options[:theme] ? "#{@options[:theme]}/" : ''
         @options[:current_page] = PageProxy.new @window_options.merge(@options), @options[:current_page], nil
+        @options[:exclude] ||= []
         #FIXME for compatibility. remove num_pages at some time in the future
         @options[:total_pages] ||= @options[:num_pages]
         @options[:num_pages] ||= @options[:total_pages]
