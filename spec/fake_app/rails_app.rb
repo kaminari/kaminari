c27625ee754f93fa03eb8d08011888eda235ade9
Akira Matsuda <ronnie@dio.jp>
2013-06-26 10:07:44 +0900

    [rails4] "config.eager_load is set to nil. Please update your config/environments/*.rb files accordingly:"

--- spec/fake_app/rails_app.rb
+++ spec/fake_app/rails_app.rb
@@ -11,6 +11,7 @@ app = Class.new(Rails::Application)
 app.config.secret_token = '3b7cd727ee24e8444053437c36cc66c4'
 app.config.session_store :cookie_store, :key => '_myapp_session'
 app.config.active_support.deprecation = :log
+app.config.eager_load = false
 # Rais.root
 app.config.root = File.dirname(__FILE__)
 Rails.backtrace_cleaner.remove_silencers!
