e17d3e01570d2e28478427037a45685d0fc112a5
Chris Olsen <olsen.chris@gmail.com>
2013-07-25 16:07:16 -0600

    remove next and prev rel attributes
    Due to a noindex nofollow meta tag currently in the head we need to
    remove the rel next/prev.  This is only a temporary hack to push any
    SEO changes to the next version.

--- lib/kaminari/helpers/action_view_extension.rb
+++ lib/kaminari/helpers/action_view_extension.rb
@@ -39,7 +39,7 @@ module Kaminari
     def link_to_previous_page(scope, name, options = {}, &block)
       params = options.delete(:params) || {}
       param_name = options.delete(:param_name) || Kaminari.config.param_name
-      link_to_unless scope.first_page?, name, params.merge(param_name => (scope.current_page - 1)), options.reverse_merge(:rel => 'previous') do
+      link_to_unless scope.first_page?, name, params.merge(param_name => (scope.current_page - 1)) do
         block.call if block
       end
     end
@@ -64,7 +64,7 @@ module Kaminari
     def link_to_next_page(scope, name, options = {}, &block)
       params = options.delete(:params) || {}
       param_name = options.delete(:param_name) || Kaminari.config.param_name
-      link_to_unless scope.last_page?, name, params.merge(param_name => (scope.current_page + 1)), options.reverse_merge(:rel => 'next') do
+      link_to_unless scope.last_page?, name, params.merge(param_name => (scope.current_page + 1)) do
         block.call if block
       end
     end
