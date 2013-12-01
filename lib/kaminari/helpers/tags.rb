a4dc7a1ed4e5c4233420a549207f0dffe335efc0
Yuki Nishijima <mail@yukinishijima.net>
2013-12-02 06:43:15 +0900

    Do not pass :param_name option in paginate method
    Do it in Tag class so we don't have to give it on the helper method.

--- lib/kaminari/helpers/tags.rb
+++ lib/kaminari/helpers/tags.rb
@@ -15,7 +15,7 @@ module Kaminari
     class Tag
       def initialize(template, options = {}) #:nodoc:
         @template, @options = template, options.dup
-        @param_name = @options.delete(:param_name)
+        @param_name = @options.delete(:param_name) || Kaminari.config.param_name
         @theme = @options[:theme] ? "#{@options.delete(:theme)}/" : ''
         @params = @options[:params] ? template.params.merge(@options.delete :params) : template.params
       end
