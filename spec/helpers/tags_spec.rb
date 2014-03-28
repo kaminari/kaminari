925d626a73d42a59160cc4403d750b078a0887d2
Yuki Nishijima <mail@yukinishijima.net>
2014-03-27 18:22:59 -0700

    Squash "warning: ambiguous first argument; put parentheses or even spaces"

--- spec/helpers/tags_spec.rb
+++ spec/helpers/tags_spec.rb
@@ -20,22 +20,22 @@ describe 'Kaminari::Helpers' do
       context "for first page" do
         subject { Tag.new(helper, :param_name => "user[page]").page_url_for(1) }
         if ActiveSupport::VERSION::STRING < "3.1.0"
-          it { should_not match /user\[page\]/ }
-          it { should match /user\[scope\]=active/ }
+          it { should_not match(/user\[page\]/) }
+          it { should match(/user\[scope\]=active/) }
         else
-          it { should_not match /user%5Bpage%5D/ }     # not match user[page]
-          it { should match /user%5Bscope%5D=active/ } #     match user[scope]=active
+          it { should_not match(/user%5Bpage%5D/) }     # not match user[page]
+          it { should match(/user%5Bscope%5D=active/) } #     match user[scope]=active
         end
       end
 
       context "for other page" do
         subject { Tag.new(helper, :param_name => "user[page]").page_url_for(2) }
         if ActiveSupport::VERSION::STRING < "3.1.0"
-          it { should match /user\[page\]=2/ }
-          it { should match /user\[scope\]=active/ }
+          it { should match(/user\[page\]=2/) }
+          it { should match(/user\[scope\]=active/) }
         else
-          it { should match /user%5Bpage%5D=2/ }       # match user[page]=2
-          it { should match /user%5Bscope%5D=active/ } # match user[scope]=active
+          it { should match(/user%5Bpage%5D=2/) }       # match user[page]=2
+          it { should match(/user%5Bscope%5D=active/) } # match user[scope]=active
         end
       end
     end
