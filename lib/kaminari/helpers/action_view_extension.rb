053bc50a9f7bf26a3a43ff0661180513ec5bf2ed
Federico Dayan <federico.dayan@gmail.com>
2014-03-31 15:31:30 -0300

    removed puts

--- lib/kaminari/helpers/action_view_extension.rb
+++ lib/kaminari/helpers/action_view_extension.rb
@@ -15,7 +15,6 @@ module Kaminari
     # * <tt>:remote</tt> - Ajax? (false by default)
     # * <tt>:ANY_OTHER_VALUES</tt> - Any other hash key & values would be directly passed into each tag as :locals value.
     def paginate(scope, options = {}, &block)
-      puts options.inspect
       paginator = Kaminari::Helpers::Paginator.new self, options.reverse_merge(:current_page => scope.current_page, :total_pages => scope.total_pages, :per_page => scope.limit_value, :param_name => Kaminari.config.param_name, :remote => false, :rel_next => "next", :rel_prev => "prev")
       paginator.to_s
     end
