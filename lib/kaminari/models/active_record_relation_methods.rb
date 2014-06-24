3f99851d135daa2ce8c249aad6ef5395b627a544
Benjamin Randles-Dunkley <benrd@quillcontent.com>
2014-06-24 20:52:56 +0100

    #572 #431 fix count issues

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -24,49 +24,21 @@ module Kaminari
         args = [column_name]
         args << options if ActiveRecord::VERSION::STRING < '4.1.0'
 
-       # If group_by is used alongside column aliases, 'count' will destroy the select part of the
-       # query to construct the count. This will cause an exception
-       # Some group_by counts return hashes, which cannot be used to calculate the number of
-       # groups.
-       # In both these cases, using a sub-query to count groups resolves the issue.
-       is_group = c.to_sql.downcase.include?(" group by ")
-       if false#is_group
-         sm = Arel::SelectManager.new c.engine
-         select_value = "count(*) as count"
-         counting_subquery = sm.project(select_value).from(c.arel.as("subquery_for_count"))
-         connection.select_one(counting_subquery)['count']
-       else
-         # .group returns an OrderedHash that responds to #count
-         c = c.count(*args)
-         if c.is_a?(Hash) || c.is_a?(ActiveSupport::OrderedHash)
-           c.count
-         else
-           c.respond_to?(:count) ? c.count(*args) : c
-         end
-       end
+        # If group_by is used alongside column aliases, 'count' will destroy the select part of the
+        # query to construct the count. This will cause an exception
+        # Some group_by counts return hashes, which cannot be used to calculate the number of
+        # groups.
+        # In both these cases, using a sub-query to count groups resolves the issue.
+        is_group = c.to_sql.downcase.include?(" group by ")
+        if is_group
+          sm = Arel::SelectManager.new c.engine
+          select_value = "count(*) as count"
+          counting_subquery = sm.project(select_value).from(c.arel.as("subquery_for_count"))
+          connection.select_one(counting_subquery)['count']
+        else
+          c.respond_to?(:count) ? c.count(*args) : c
+        end
       end
     end
   end
 end
-
-
-# # If group_by is used alongside column aliases, 'count' will destroy the select part of the
-# # query to construct the count. This will cause an exception
-# # Some group_by counts return hashes, which cannot be used to calculate the number of
-# # groups.
-# # In both these cases, using a sub-query to count groups resolves the issue.
-# is_group = c.to_sql.downcase.include?(" group by ")
-# if is_group
-#   sm = Arel::SelectManager.new c.engine
-#   select_value = "count(*) as count"
-#   counting_subquery = sm.project(select_value).from(c.arel.as("subquery_for_count"))
-#   connection.select_one(counting_subquery)['count']
-# else
-#   # # .group returns an OrderedHash that responds to #count
-#   # c = c.count(*args)
-#   # if c.is_a?(Hash) || c.is_a?(ActiveSupport::OrderedHash)
-#   #   c.count
-#   # else
-#     c.respond_to?(:count) ? c.count(*args) : c
-#   end
-# # end
