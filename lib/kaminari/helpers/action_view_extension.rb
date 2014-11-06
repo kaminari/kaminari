a7b6c338f53fdfa820aea3f72fc7ffedcc291946
Yuki Nishijima <mail@yukinishijima.net>
2014-11-05 20:58:20 -0800

    Restore backwards compatibility of #num_pages in #paginate
    Conflicts:
    	lib/kaminari/helpers/action_view_extension.rb

--- lib/kaminari/helpers/action_view_extension.rb
+++ lib/kaminari/helpers/action_view_extension.rb
@@ -15,7 +15,9 @@ module Kaminari
     # * <tt>:remote</tt> - Ajax? (false by default)
     # * <tt>:ANY_OTHER_VALUES</tt> - Any other hash key & values would be directly passed into each tag as :locals value.
     def paginate(scope, options = {}, &block)
-      paginator = Kaminari::Helpers::Paginator.new self, options.reverse_merge(:current_page => scope.current_page, :total_pages => scope.total_pages, :per_page => scope.limit_value, :remote => false)
+      options[:total_pages] ||= options[:num_pages] || scope.total_pages
+
+      paginator = Kaminari::Helpers::Paginator.new(self, options.reverse_merge(:current_page => scope.current_page, :per_page => scope.limit_value, :remote => false))
       paginator.to_s
     end
 
