7ebce25c8c775cfa70b297624df8f2e066ae69f7
Yuki Nishijima <mail@yukinishijima.net>
2014-12-14 21:05:47 -0800

    Remove #define_scope module function

--- lib/kaminari/models/mongoid_extension.rb
+++ lib/kaminari/models/mongoid_extension.rb
@@ -7,28 +7,19 @@ module Kaminari
       include Kaminari::ConfigurationMethods
 
       included do
-        Kaminari::MongoidExtension::Document.send(:define_scope, self)
+        scope Kaminari.config.page_method_name, Proc.new {|num|
+          limit(default_per_page).offset(default_per_page * ([num.to_i, 1].max - 1))
+        } do
+          include Kaminari::MongoidCriteriaMethods
+          include Kaminari::PageScopeMethods
+        end
 
         class << self
           def inherited_with_kaminari(kls)
             inherited_without_kaminari(kls)
-            Kaminari::MongoidExtension::Document.send(:define_scope, kls)
+            kls.send(:include, Kaminari::MongoidExtension::Document.dup)
           end
           alias_method_chain :inherited, :kaminari
-        end
-      end
-
-      private
-      def self.define_scope(kls)
-        kls.class_eval do
-          # Fetch the values at the specified page number
-          # Model.page(5)
-          scope Kaminari.config.page_method_name, Proc.new {|num|
-            limit(default_per_page).offset(default_per_page * ([num.to_i, 1].max - 1))
-          } do
-            include Kaminari::MongoidCriteriaMethods
-            include Kaminari::PageScopeMethods
-          end
         end
       end
     end
