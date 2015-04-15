c21d5a3f51b679b4a7b043c53663b5f69cf69284
André Andrade <andrepalasandrade@gmail.com>
2015-04-15 17:46:24 +0100

    fix #672 - correctly load the page method name when changed for external model classes

--- lib/kaminari/railtie.rb
+++ lib/kaminari/railtie.rb
@@ -1,7 +1,15 @@
 module Kaminari
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'kaminari' do |_app|
-      Kaminari::Hooks.init
+      if Rails.env.test?
+        ActiveSupport.on_load(:active_record) do
+          ::ActiveRecord::Base.send :include, Kaminari::ConfigurationMethods
+        end
+      end
+
+      config.after_initialize do
+        Kaminari::Hooks.init
+      end
     end
   end
 end
