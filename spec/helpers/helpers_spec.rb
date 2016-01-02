8294c1b5a7906df3d833a25d2849512bb3a1ec61
Yuki Nishijima <mail@yukinishijima.net>
2016-01-02 11:35:30 +0000

    Fix a failing build for Rails edge

--- spec/helpers/helpers_spec.rb
+++ spec/helpers/helpers_spec.rb
@@ -26,7 +26,7 @@ describe 'Kaminari::Helpers::Paginator' do
       @paginator = Paginator.new(template, :params => {:controller => 'foo', :action => 'bar'})
     end
     subject { @paginator.page_tag(template).instance_variable_get('@params') }
-    it { should == {:controller => 'foo', :action => 'bar'} }
+    it { should == {'controller' => 'foo', 'action' => 'bar'} }
   end
 
   describe '#param_name' do
