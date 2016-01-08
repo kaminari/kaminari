b2d72a7ec6332513750cb3e5b2349fe1fe945a4e
Yuki Nishijima <mail@yukinishijima.net>
2016-01-08 21:43:53 +0900

    Merge pull request #766 from audionerd/765-fix-mangled-params-in-pagination-links

--- lib/kaminari/helpers/tags.rb
+++ lib/kaminari/helpers/tags.rb
@@ -19,9 +19,9 @@ module Kaminari
         @theme = @options.delete(:theme)
         @views_prefix = @options.delete(:views_prefix)
         @params = @options[:params] ? template.params.merge(@options.delete :params) : template.params
-        # @params in Rails 5 does no more inherits from Hash but composes a Hash
-        if @params.instance_variable_defined?(:@parameters) && !@params.respond_to?(:deep_merge)
-          @params = @params.instance_variable_get :@parameters
+        # @params in Rails 5 no longer inherits from Hash
+        if @params.respond_to?(:to_unsafe_h)
+          @params = @params.to_unsafe_h
         else
           @params = @params.with_indifferent_access
         end
