5adc533233425a7b738b1aa48371579274c4953b
Akira Matsuda <ronnie@dio.jp>
2016-04-02 20:50:32 +0900

    Move mongoid cleaner settings to kaminari-mongoid gem

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,10 +1,8 @@
 DatabaseCleaner[:active_record].strategy = :transaction if defined? ActiveRecord
-DatabaseCleaner[:mongoid].strategy = :truncation if defined? Mongoid
 
 RSpec.configure do |config|
   config.before :suite do
     DatabaseCleaner.clean_with :truncation if defined? ActiveRecord
-    DatabaseCleaner.clean_with :truncation if defined? Mongoid
   end
   config.before :each do
     DatabaseCleaner.start
