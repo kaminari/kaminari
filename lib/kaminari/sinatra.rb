224a1f977a5a152f3f68b8c811caf004849c6496
Akira Matsuda <ronnie@dio.jp>
2016-04-15 02:23:26 +0900

    Extract kaminari-sinatra gem, and warn users to use the gemified adapter
    The built-in Sinatra support will be removed from this gem in the 1.0 release

--- lib/kaminari/sinatra.rb
+++ lib/kaminari/sinatra.rb
@@ -2,4 +2,6 @@ require 'sinatra/base'
 require 'kaminari'
 require 'kaminari/helpers/sinatra_helpers'
 
+ActiveSupport::Deprecation.warn 'Kaminari Sinatra support has been extracted to a separate gem, and will be removed in the next 1.0 release. Please bundle kaminari-sinatra gem.'
+
 Kaminari::Hooks.init
