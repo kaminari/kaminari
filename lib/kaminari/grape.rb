f3bf4382ccb47ec5b0be67539af7dca2ff8e982d
Akira Matsuda <ronnie@dio.jp>
2016-06-13 09:56:16 +0900

    Extract kaminari-grape gem, and warn users to use the gemified adapter
    The built-in Grape support will be removed from this gem in the 1.0 release

--- lib/kaminari/grape.rb
+++ lib/kaminari/grape.rb
@@ -1,4 +1,6 @@
 require 'grape'
 require 'kaminari'
 
+ActiveSupport::Deprecation.warn 'Kaminari Grape support has been extracted to a separate gem, and will be removed in the next 1.0 release. Please bundle kaminari-grape gem.'
+
 Kaminari::Hooks.init
