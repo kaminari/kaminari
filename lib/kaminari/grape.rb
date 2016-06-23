eda17817fc31a1de4fab36a842f17db4d9fd5905
Akira Matsuda <ronnie@dio.jp>
2016-06-23 16:37:51 +0900

    Remove Grape support that has been extracted to kaminari-grape gem

--- lib/kaminari/grape.rb
+++
@@ -1,6 +0,0 @@
-require 'grape'
-require 'kaminari'
-
-ActiveSupport::Deprecation.warn 'Kaminari Grape support has been extracted to a separate gem, and will be removed in the next 1.0 release. Please bundle kaminari-grape gem.'
-
-Kaminari::Hooks.init
