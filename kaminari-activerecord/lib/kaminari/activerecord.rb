27b9f6dcea8b27ba560b7fa4c46ce328042c52a3
Akira Matsuda <ronnie@dio.jp>
2016-06-30 18:21:55 +0900

    bundle gem kaminari-activerecord

---
+++ kaminari-activerecord/lib/kaminari/activerecord.rb
@@ -0,0 +1,7 @@
+require "kaminari/activerecord/version"
+
+module Kaminari
+  module Activerecord
+    # Your code goes here...
+  end
+end
---
+++ kaminari-activerecord/lib/kaminari/activerecord/version.rb
@@ -0,0 +1,5 @@
+module Kaminari
+  module Activerecord
+    VERSION = "0.1.0"
+  end
+end
