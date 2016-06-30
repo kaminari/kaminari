0ccbe2bc9dd0a1f173dde6fa64fc536b40638f26
Akira Matsuda <ronnie@dio.jp>
2016-06-30 23:21:55 +0900

    There can be apps that use Action View off Rails

--- kaminari-actionview/lib/kaminari/actionview.rb
+++ kaminari-actionview/lib/kaminari/actionview.rb
@@ -1,2 +1,6 @@
 require "kaminari/actionview/version"
-require 'kaminari/actionview/railtie'
+
+ActiveSupport.on_load :action_view do
+  require 'kaminari/actionview/action_view_extension'
+  ::ActionView::Base.send :include, Kaminari::ActionViewExtension
+end
