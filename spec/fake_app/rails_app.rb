7d3dbd8e396ddc30454d1bf156a00f21ed9adaf6
Akira Matsuda <ronnie@dio.jp>
2016-07-05 17:54:06 +0900

    "DEPRECATION WARNING: You didn't set `secret_key_base`."
    "Read the upgrade documentation to learn more about this new config option."

--- spec/fake_app/rails_app.rb
+++ spec/fake_app/rails_app.rb
@@ -8,7 +8,7 @@ require 'fake_app/mongoid/config' if defined? Mongoid
 require 'fake_app/mongo_mapper/config' if defined? MongoMapper
 # config
 app = Class.new(Rails::Application)
-app.config.secret_token = '3b7cd727ee24e8444053437c36cc66c4'
+app.config.secret_key_base = app.config.secret_token = '3b7cd727ee24e8444053437c36cc66c4'
 app.config.session_store :cookie_store, :key => '_myapp_session'
 app.config.active_support.deprecation = :log
 app.config.eager_load = false
