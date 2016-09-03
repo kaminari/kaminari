26a8532ff2d932f9275c353e1b41241449c8adaf
Patrice Lebel <patleb@users.noreply.github.com>
2016-09-03 15:11:14 -0400

    allow to override total_count in the model class

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -19,23 +19,43 @@ module Kaminari
 
     def total_count(column_name = :all, options = {}) #:nodoc:
       # #count overrides the #select which could include generated columns referenced in #order, so skip #order here, where it's irrelevant to the result anyway
-      @total_count ||= begin
-        c = except(:offset, :limit, :order)
+      @total_count ||= if @klass.respond_to? :total_count
+        # allow overriding total_count at the class level (might be necessary performance-wise)
+        #
+        # def total_count(column_name = :all, options = {})
+        #   if options[:relation].values.has_key? :where
+        #     false
+        #   else
+        #     AnyModel.sum(:other_records_count)
+        #   end
+        # end
+        #
+        result = @klass.total_count(column_name, options.merge(relation: self))
+        # return false from the override if you need the default behavior
+        result ? result : _total_count(column_name, options)
+      else
+        _total_count(column_name, options)
+      end
+    end
 
-        # Remove includes only if they are irrelevant
-        c = c.except(:includes) unless references_eager_loaded_tables?
+    private
 
-        # Rails 4.1 removes the `options` argument from AR::Relation#count
-        args = [column_name]
-        args << options if ActiveRecord::VERSION::STRING < '4.1.0'
+    def _total_count(column_name = :all, options = {}) #:nodoc:
+      c = except(:offset, :limit, :order)
 
-        # .group returns an OrderdHash that responds to #count
-        c = c.count(*args)
-        if c.is_a?(Hash) || c.is_a?(ActiveSupport::OrderedHash)
-          c.count
-        else
-          c.respond_to?(:count) ? c.count(*args) : c
-        end
+      # Remove includes only if they are irrelevant
+      c = c.except(:includes) unless references_eager_loaded_tables?
+
+      # Rails 4.1 removes the `options` argument from AR::Relation#count
+      args = [column_name]
+      args << options if ActiveRecord::VERSION::STRING < '4.1.0'
+
+      # .group returns an OrderdHash that responds to #count
+      c = c.count(*args)
+      if c.is_a?(Hash) || c.is_a?(ActiveSupport::OrderedHash)
+        c.count
+      else
+        c.respond_to?(:count) ? c.count(*args) : c
       end
     end
   end
