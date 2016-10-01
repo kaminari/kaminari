6534e33c36b115ebe2e3e668650550562934d147
Akira Matsuda <ronnie@dio.jp>
2016-10-01 23:36:29 +0900

    Revert "Better way of suppressing render_partial logs"
    This reverts commit 0fcfad2030c3558471f6136339a06b87c3d888f2.
    
    This hides all logs within this `render` call.

--- lib/kaminari/helpers/action_view_extension.rb
+++ lib/kaminari/helpers/action_view_extension.rb
@@ -140,10 +140,32 @@ module Kaminari
         undef :to_s
         # Redefining to_s not to log each render_partial
         def to_s #:nodoc:
-          if (logger = ActionView::Base.logger)
-            logger.silence { super @window_options.merge paginator: self }
+          subscriber = ActionView::LogSubscriber.log_subscribers.detect {|ls| ls.is_a? ActionView::LogSubscriber}
+
+          # There is a logging subscriber
+          # and we don't want it to log render_partial
+          # It is threadsafe, but might not repress logging
+          # consistently in a high-load environment
+          if subscriber
+            unless defined? subscriber.render_partial_with_logging
+              class << subscriber
+                alias_method :render_partial_with_logging, :render_partial
+                attr_accessor :render_without_logging
+                # ugly hack to make a renderer where
+                # we can turn logging on or off
+                def render_partial(event)
+                  render_partial_with_logging(event) unless render_without_logging
+                end
+              end
+            end
+
+            subscriber.render_without_logging = true
+            ret = super @window_options.merge :paginator => self
+            subscriber.render_without_logging = false
+
+            ret
           else
-            super @window_options.merge paginator: self
+            super @window_options.merge :paginator => self
           end
         end
       end
