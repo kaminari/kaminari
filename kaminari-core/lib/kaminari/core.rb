bb0226c07571184fed8a9e78dc9dc0a7cb1d0208
Akira Matsuda <ronnie@dio.jp>
2016-10-04 21:11:43 +0900

    Move core code to kaminari-core gem

--- kaminari-core/lib/kaminari/core.rb
+++ kaminari-core/lib/kaminari/core.rb
@@ -1,7 +1,42 @@
-require "kaminari/core/version"
-
 module Kaminari
-  module Core
-    # Your code goes here...
-  end
+end
+
+# load Rails/Railtie
+begin
+  require 'rails'
+rescue LoadError
+  #do nothing
+end
+
+$stderr.puts <<-EOC if !defined?(::Rails::Railtie) && !defined?(Sinatra) && !defined?(Grape)
+warning: no framework detected.
+
+Your Gemfile might not be configured properly.
+---- e.g. ----
+Rails:
+    gem 'kaminari'
+
+Sinatra/Padrino:
+    gem 'kaminari-sinatra'
+
+Grape:
+    gem 'kaminari-grape'
+
+EOC
+
+# load Kaminari components
+require 'kaminari/config'
+require 'kaminari/exceptions'
+require 'kaminari/helpers/paginator'
+require 'kaminari/models/page_scope_methods'
+require 'kaminari/models/configuration_methods'
+require 'kaminari/hooks'
+
+require 'kaminari/actionview'
+require 'kaminari/activerecord'
+
+# if not using Railtie, call `Kaminari::Hooks.init` directly
+if defined? ::Rails::Railtie
+  require 'kaminari/railtie'
+  require 'kaminari/engine'
 end
