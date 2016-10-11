7d49a6cf3cc79a6113c9b1e68e1ba80753480281
Akira Matsuda <ronnie@dio.jp>
2016-10-11 22:31:32 +0900

    No instance methods for a Module

--- kaminari-core/lib/kaminari/config.rb
+++ kaminari-core/lib/kaminari/config.rb
@@ -7,16 +7,16 @@ module Kaminari
   #   end
   include ActiveSupport::Configurable
 
-  config_accessor(:default_per_page) { 25 }
-  config_accessor(:max_per_page)
-  config_accessor(:window) { 4 }
-  config_accessor(:outer_window) { 0 }
-  config_accessor(:left) { 0 }
-  config_accessor(:right) { 0 }
-  config_accessor(:page_method_name) { :page }
-  config_accessor(:param_name) { :page }
-  config_accessor(:max_pages)
-  config_accessor(:params_on_first_page) { false }
+  config_accessor(:default_per_page, instance_accessor: false) { 25 }
+  config_accessor(:max_per_page, instance_accessor: false)
+  config_accessor(:window, instance_accessor: false) { 4 }
+  config_accessor(:outer_window, instance_accessor: false) { 0 }
+  config_accessor(:left, instance_accessor: false) { 0 }
+  config_accessor(:right, instance_accessor: false) { 0 }
+  config_accessor(:page_method_name, instance_accessor: false) { :page }
+  config_accessor(:param_name, instance_accessor: false) { :page }
+  config_accessor(:max_pages, instance_accessor: false)
+  config_accessor(:params_on_first_page, instance_accessor: false) { false }
 
   # If param_name was given as a callable object, call it when returning
   config.undef_method :param_name
