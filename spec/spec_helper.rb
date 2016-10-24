8ed2f84c2d31f95d5a229110bfac6bed6257dedb
Akira Matsuda <ronnie@dio.jp>
2016-10-24 23:07:03 +0900

    ActiveRecord should always be defined here

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ require 'capybara/rspec'
 require 'database_cleaner'
 
 # Simulate a gem providing a subclass of ActiveRecord::Base before the Railtie is loaded.
-require 'fake_gem' if defined? ActiveRecord
+require 'fake_gem'
 
 if defined? ::Rails::Railtie
   require 'fake_app/rails_app'
