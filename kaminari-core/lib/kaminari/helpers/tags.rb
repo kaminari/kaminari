647eb5857dc2cb4ffee5f5eea66e3822d8edcf91
Akira Matsuda <ronnie@dio.jp>
2016-10-24 20:54:13 +0900

    Kwargsifying `params` parameter for Tag.new

--- kaminari-core/lib/kaminari/helpers/tags.rb
+++ kaminari-core/lib/kaminari/helpers/tags.rb
@@ -16,14 +16,14 @@ module Kaminari
     # installed template will be used.
     #   e.g.)  Paginator  ->  $GEM_HOME/kaminari-x.x.x/app/views/kaminari/_paginator.html.erb
     class Tag
-      def initialize(template, param_name: nil, theme: nil, views_prefix: nil, **options) #:nodoc:
+      def initialize(template, params: {}, param_name: nil, theme: nil, views_prefix: nil, **options) #:nodoc:
         @template, @options = template, options.dup
         @param_name = param_name || Kaminari.config.param_name
         @theme, @views_prefix = theme, views_prefix
         @params = template.params
         # @params in Rails 5 no longer inherits from Hash
         @params = @params.to_unsafe_h if @params.respond_to?(:to_unsafe_h)
-        @params = @params.with_indifferent_access.except(*PARAM_KEY_BLACKLIST).merge(@options.delete(:params) || {})
+        @params = @params.with_indifferent_access.except(*PARAM_KEY_BLACKLIST).merge(params)
       end
 
       def to_s(locals = {}) #:nodoc:
