fe4b0a141110d277e69cfb8e754414d33e8873df
Akira Matsuda <ronnie@dio.jp>
2016-11-24 18:35:18 +0900

    Move ActionView independent helpers code to a separate file

--- kaminari-actionview/lib/kaminari/actionview.rb
+++ kaminari-actionview/lib/kaminari/actionview.rb
@@ -2,6 +2,8 @@
 require "kaminari/actionview/version"
 
 ActiveSupport.on_load :action_view do
+  require 'kaminari/helpers/helper_methods'
+  ::ActionView::Base.send :include, Kaminari::Helpers::HelperMethods
+
   require 'kaminari/actionview/action_view_extension'
-  ::ActionView::Base.send :include, Kaminari::ActionViewExtension
 end
