ba5ad3b9f9d045553b4aa52b74541427a8ff1ae9
Akira Matsuda <ronnie@dio.jp>
2016-12-02 10:59:14 +0900

    Refactoring @total_count memoization not to use block for later refactoring

--- kaminari-activerecord/lib/kaminari/activerecord/active_record_relation_methods.rb
+++ kaminari-activerecord/lib/kaminari/activerecord/active_record_relation_methods.rb
@@ -12,18 +12,18 @@ module Kaminari
     end
 
     def total_count(column_name = :all, _options = nil) #:nodoc:
-      @total_count ||= begin
-        # #count overrides the #select which could include generated columns referenced in #order, so skip #order here, where it's irrelevant to the result anyway
-        c = except(:offset, :limit, :order)
-        # Remove includes only if they are irrelevant
-        c = c.except(:includes) unless references_eager_loaded_tables?
-        # .group returns an OrderedHash that responds to #count
-        c = c.count(column_name)
-        if c.is_a?(Hash) || c.is_a?(ActiveSupport::OrderedHash)
-          c.count
-        else
-          c.respond_to?(:count) ? c.count(column_name) : c
-        end
+      return @total_count if @total_count
+
+      # #count overrides the #select which could include generated columns referenced in #order, so skip #order here, where it's irrelevant to the result anyway
+      c = except(:offset, :limit, :order)
+      # Remove includes only if they are irrelevant
+      c = c.except(:includes) unless references_eager_loaded_tables?
+      # .group returns an OrderedHash that responds to #count
+      c = c.count(column_name)
+      @total_count = if c.is_a?(Hash) || c.is_a?(ActiveSupport::OrderedHash)
+        c.count
+      else
+        c.respond_to?(:count) ? c.count(column_name) : c
       end
     end
   end
