bd8fabafe763ec5f46d6a4c0e000214f684a901a
Akira Matsuda <ronnie@dio.jp>
2016-12-08 16:25:08 +0900

    :golf:

--- kaminari-core/lib/kaminari/helpers/helper_methods.rb
+++ kaminari-core/lib/kaminari/helpers/helper_methods.rb
@@ -106,9 +106,7 @@ module Kaminari
         if collection.total_pages < 2
           t('helpers.page_entries_info.one_page.display_entries', entry_name: entry_name, count: collection.total_count)
         else
-          first = collection.offset_value + 1
-          last = (sum = collection.offset_value + collection.limit_value) > collection.total_count ? collection.total_count : sum
-          t('helpers.page_entries_info.more_pages.display_entries', entry_name: entry_name, first: first, last: last, total: collection.total_count)
+          t('helpers.page_entries_info.more_pages.display_entries', entry_name: entry_name, first: collection.offset_value + 1, last: [collection.offset_value + collection.limit_value, collection.total_count].min, total: collection.total_count)
         end.html_safe
       end
 
