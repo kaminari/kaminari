db62fb33225d18a222c348aa0d916eea317d8bd4
Akira Matsuda <ronnie@dio.jp>
2017-01-10 22:29:39 +0900

    Version 1.0.0

--- kaminari-core/lib/kaminari/core/version.rb
+++ kaminari-core/lib/kaminari/core/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Kaminari
   module Core
-    VERSION = '1.0.0.rc1'
+    VERSION = '1.0.0'
   end
 end
--- lib/kaminari/version.rb
+++ lib/kaminari/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 module Kaminari
-  VERSION = '1.0.0.rc1'
+  VERSION = '1.0.0'
 end
