070a9a8159ce9f0f1e908d10c198da247b4246d1
Akira Matsuda <ronnie@dio.jp>
2017-05-18 18:33:04 +0900

    Simpler AR configuration
    because Rails.env isn't always 'test' (unless explicitly given)

--- kaminari-core/test/fake_app/active_record/config.rb
+++ kaminari-core/test/fake_app/active_record/config.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 # database
-ActiveRecord::Base.configurations = {'test' => {adapter: 'sqlite3', database: ':memory:'}}
-ActiveRecord::Base.establish_connection :test
+ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
