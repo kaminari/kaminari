1ccdaabac414a7bf1dda7666006e1e34ec5e68ca
Akira Matsuda <ronnie@dio.jp>
2017-06-02 19:13:10 +0900

    Use physical .sqlite3 file instead of in-memory DB

--- kaminari-core/test/fake_app/active_record/config.rb
+++ kaminari-core/test/fake_app/active_record/config.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 # database
-ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
+ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: 'kaminari_test.sqlite3'
