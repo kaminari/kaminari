7ec9b75d0d12e9d3f0c788c8518a5592f44d00e6
Akira Matsuda <ronnie@dio.jp>
2021-12-20 00:20:04 +0900

    Monkey-patch test-unit-rails not to raise NameError from triple-nested sub_test_case
    see: https://github.com/test-unit/test-unit-rails/pull/24 and https://github.com/test-unit/test-unit/pull/207

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,3 +22,27 @@ require 'test/unit/rails/test_help'
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.join(Gem.loaded_specs['kaminari-core'].gem_dir, 'test')}/support/**/*.rb"].each {|f| require f}
+
+# Monkey-patching test-unit-rails not to raise NameError from triple-nested sub_test_case
+ActionView::TestCase.class_eval do
+  class << self
+    def sub_test_case(name, &block)
+      parent_test_case = self
+      sub_test_case = Class.new(self) do
+        singleton_class = class << self; self; end
+        singleton_class.__send__(:define_method, :name) do
+          [parent_test_case.name, name].compact.join("::")
+        end
+        singleton_class.__send__(:define_method, :default_helper_module!) do
+          begin
+            super()
+          rescue NameError
+            # Anonymous classes generated via sub_test_case may not always follow Ruby module name convention
+          end
+        end
+      end
+      sub_test_case.class_eval(&block)
+      sub_test_case
+    end
+  end
+end
