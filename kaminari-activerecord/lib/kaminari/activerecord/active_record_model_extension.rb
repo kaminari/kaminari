e0e93f78888046a03584026d67f3159566adab5e
张小辉 <xiaohui@tanmer.com>
2022-06-16 22:46:56 +0800

    [Security] ignore hash value from params[:page]

--- kaminari-activerecord/lib/kaminari/activerecord/active_record_model_extension.rb
+++ kaminari-activerecord/lib/kaminari/activerecord/active_record_model_extension.rb
@@ -13,6 +13,7 @@ module Kaminari
       #   Model.page(5)
       eval <<-RUBY, nil, __FILE__, __LINE__ + 1
         def self.#{Kaminari.config.page_method_name}(num = nil)
+          num = num.respond_to?(:to_i) ? num.to_i : 1
           per_page = max_per_page && (default_per_page > max_per_page) ? max_per_page : default_per_page
           limit(per_page).offset(per_page * ((num = num.to_i - 1) < 0 ? 0 : num)).extending do
             include Kaminari::ActiveRecordRelationMethods
